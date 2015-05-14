ebe1ae130f3111d77574c0cb5e9b543ce302bada
Princess Sampson <ps@kingonomics.com>
2015-05-14 15:17:21 -0400

    first question done

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log(items);
+
+// we used this to get a list of prices in class
+
+// var answer1 = document.querySelector('#answer1');
+
+// answer1.innerText = '';
+
+// items.forEach( function (item) {
+//   answer1.appendChild(document.createTextNode(item.price + '\n'));
+// });
+
+
+//QUESTION ONE
+
+//get all prices
+var allPrices = items.map( function (item) {
+
+  return item.price;
+
+});
+
+//get the sum
+
+var sum = allPrices.reduce( function (previous, current) {
+return previous + current;
+
+});
+
+//average
+var average = sum / allPrices.length;
+
+// answer
+var answer = average.toFixed(2);
+
+//drop answer in DOM Node
+document.querySelector('#answer1').textContent = "The average price is $" + answer;
+
+console.log("The average price is $" + answer);
+//END QUESTION ONE 
